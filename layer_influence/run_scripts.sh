d06ca9a2973408eef54884cda5e196e299192697
aamer98 <ar.aamer@gmail.com>
2025-02-22 22:44:24 +0000

    layer pruning

--- layer_influence/run_scripts.sh
+++ layer_influence/run_scripts.sh
@@ -1,3 +1,16 @@
-CUDA_VISIBLE_DEVICES=7 python layer_influence/evaluate_layer_influence.py --dataset "openwebtext" --model-name "llama-2" --model-size 7b --batch-size 1 --sequence-length 2048 --subsample-size 200 --pruning-scheme "both" --wandb-project 'layer_influence'
 
-CUDA_VISIBLE_DEVICES=7 python layer_influence/evaluate_layer_influence.py --dataset "openwebtext" --model-name "qwen2" --model-size 7b --batch-size 1 --sequence-length 2048 --subsample-size 200 --pruning-scheme "both" --wandb-project 'layer_influence'
+# CUDA_VISIBLE_DEVICES=7 python layer_influence/evaluate_layer_influence.py --dataset "openwebtext" --model_name "qwen2" --model_size 0.5b --batch_size 8 --sequence_length 2048 --subsample_size 250000 --pruning_scheme "both" --wandb_project 'layer_influence'
+
+CUDA_VISIBLE_DEVICES=1 python /home/aamer/repos/wanda/layer_influence/evaluate_layer_influence.py --dataset "wikitext-2" --model_name "qwen2" --model_size 0.5b --batch_size 8 --sequence_length 2048 --subsample_size 250000 --pruning_scheme "both" --wandb_project 'layer_influence' &
+
+CUDA_VISIBLE_DEVICES=2 python /home/aamer/repos/wanda/layer_influence/evaluate_layer_influence.py --dataset "pg19" --model_name "qwen2" --model_size 0.5b --batch_size 8 --sequence_length 2048 --subsample_size 250000 --pruning_scheme "both" --wandb_project 'layer_influence' &
+
+CUDA_VISIBLE_DEVICES=3 python /home/aamer/repos/wanda/layer_influence/evaluate_layer_influence.py --dataset "cc_news" --model_name "qwen2" --model_size 0.5b --batch_size 8 --sequence_length 2048 --subsample_size 250000 --pruning_scheme "both" --wandb_project 'layer_influence' &
+
+CUDA_VISIBLE_DEVICES=4 python /home/aamer/repos/wanda/layer_influence/evaluate_layer_influence.py --dataset "bookcorpus" --model_name "qwen2" --model_size 0.5b --batch_size 8 --sequence_length 2048 --subsample_size 250000 --pruning_scheme "both" --wandb_project 'layer_influence' &
+
+CUDA_VISIBLE_DEVICES=5 python /home/aamer/repos/wanda/layer_influence/evaluate_layer_influence.py --dataset "c4" --model_name "qwen2" --model_size 0.5b --batch_size 8 --sequence_length 2048 --subsample_size 250000 --pruning_scheme "both" --wandb_project 'layer_influence' &
+
+CUDA_VISIBLE_DEVICES=6 python /home/aamer/repos/wanda/layer_influence/evaluate_layer_influence.py --dataset "slimpajama" --model_name "qwen2" --model_size 0.5b --batch_size 8 --sequence_length 2048 --subsample_size 250000 --pruning_scheme "both" --wandb_project 'layer_influence' &
+
+# CUDA_VISIBLE_DEVICES=7 python layer_influence/evaluate_layer_influence.py --dataset "wikitext-2" --model_name "qwen2" --model_size 1.5b --batch_size 4 --sequence_length 2048 --subsample_size 250000 --pruning_scheme "both" --wandb_project 'layer_influence' &
